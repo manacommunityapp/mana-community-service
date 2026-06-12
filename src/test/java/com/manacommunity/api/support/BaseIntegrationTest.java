a2c2a59c40b2e5be2d510f30b680703677a93df5
manacommunityapp <manacommunityapp@gmail.com>
2026-06-12 22:04:59 +0530

    changes for aws cicd deployment

--- src/test/java/com/manacommunity/api/support/BaseIntegrationTest.java
+++ src/test/java/com/manacommunity/api/support/BaseIntegrationTest.java
@@ -3,7 +3,7 @@ package com.manacommunity.api.support;
 import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+//import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -59,8 +59,8 @@ public abstract class BaseIntegrationTest {
     @LocalServerPort
     protected int port;
 
-    @Autowired
-    protected TestRestTemplate restTemplate;
+    //@Autowired
+    //protected TestRestTemplate restTemplate;
 
     protected String baseUrl(String path) {
         return "http://localhost:" + port + path;
