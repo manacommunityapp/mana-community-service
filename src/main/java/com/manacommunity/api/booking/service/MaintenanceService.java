fe17e251a5348fc9451df7dc8ff766ab0db7c15b
manacommunityapp <manacommunityapp@gmail.com>
2026-07-31 23:32:02 +0530

    module fix changes

--- src/main/java/com/manacommunity/api/booking/service/MaintenanceService.java
+++ src/main/java/com/manacommunity/api/booking/service/MaintenanceService.java
@@ -18,7 +18,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-@Service
+@Service("bookingMaintenanceService")
 @RequiredArgsConstructor
 public class MaintenanceService {
 
